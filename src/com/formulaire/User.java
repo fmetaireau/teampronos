728ce301b8d59cc4e7a98b0d16c204e688486fdc
Metaireau Florian <fmetaireau@asi.fr>
2019-10-07 11:44:25 +0200

    Initialisation profil (inscription et connexion)

---
+++ src/com/formulaire/User.java
@@ -0,0 +1,29 @@
+package com.formulaire;
+
+public class User {
+
+    private String email;
+    private String motDePasse;
+    private String nom;
+
+    public void setEmail(String email) {
+	this.email = email;
+    }
+    public String getEmail() {
+	return email;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+	this.motDePasse = motDePasse;
+    }
+    public String getMotDePasse() {
+	return motDePasse;
+    }
+
+    public void setNom(String nom) {
+	this.nom = nom;
+    }
+    public String getNom() {
+	return nom;
+    }
+}
---
+++ src/com/servlets/InscriptionServlet.java
@@ -0,0 +1,37 @@
+package com.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.formulaire.InscriptionForm;
+import com.formulaire.User;
+
+
+public class InscriptionServlet extends HttpServlet {
+    public static final String ATT_USER = "utilisateur";
+    public static final String ATT_FORM = "form";
+    public static final String VUE = "/jsp/profil/inscription.jsp";
+		
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+        /* Affichage de la page d'inscription */
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    }
+	
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+        /* Préparation de l'objet formulaire */
+        InscriptionForm form = new InscriptionForm();
+		
+        /* Appel au traitement et à la validation de la requête, et récupération du bean en résultant */
+        User utilisateur = form.inscrireUtilisateur( request );
+		
+        /* Stockage du formulaire et du bean dans l'objet request */
+        request.setAttribute( ATT_FORM, form );
+        request.setAttribute( ATT_USER, utilisateur );
+		
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    }
+}
